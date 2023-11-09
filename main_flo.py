face918dcea55a48fb1e0fc681b2b3adda659a65
Florian MOREL <florian.morel@ensta-paris.fr>
2023-11-09 14:57:58 +0100

    ouais

--- main_flo.py
+++ main_flo.py
@@ -14,10 +14,36 @@ wind_turbines = data["wind_turbines"]
 wind_scenarios = data["wind_scenarios"]
 param = data["general_parameters"]
 
+nb_s = len(s_loc)
+nb_t = len(wind_turbines)
+
+print("nb_s", nb_s)
+print("nb_t", nb_t)
+
 
 def cplexsolve():
     # MODEL
     model = CpoModel(name="sujet6-kiro")
+
+    # VARIABLES
+    substations = [
+        model.integer_var_dict(
+            ["type_s", "type_c", "linked_s"],
+            min=0,
+            name="substation" + str(i),
+        )
+        for i in range(nb_s)
+    ]
+    z_cables = [
+        model.integer_var_dict(
+            ["s_id"],
+            min=0,
+            name="z_" + str(i),
+        )
+        for i in range(nb_t)
+    ]
+
+    # CONSTRAINTS
 
     # SOLVE
     model.solve(TimeLimit=10)
