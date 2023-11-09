0e46fce0de6f118b58bbb28cf70dec599abc99aa
Florian MOREL <florian.morel@ensta-paris.fr>
2023-11-09 17:11:28 +0100

    mm

--- main_thomas.py
+++ main_thomas.py
@@ -3,8 +3,6 @@ import numpy as np
 from docplex.cp.model import *
 
 
-<<<<<<< HEAD
-=======
 with open("./instances/toy.json") as json_file:
     data = json.load(json_file)
 
@@ -21,10 +19,49 @@ nb_t = len(wind_turbines)
 
 print("nb_s", nb_s)
 print("nb_t", nb_t)
-print("len(s_type)",len("s_type"))
-print("n_land_s_cable",len("land_s_cables"))
+print("len(s_type)", len("s_type"))
+print("n_land_s_cable", len("land_s_cables"))
 
->>>>>>> 153bfcf52707dece824ae32bc3ff82d0ec5da668
+
+def const_cost():
+    c_cost = 0
+    # construction substation
+    for sp, substation in enumerate(substations):
+        for s in range(nb_s):
+            c_cost += (
+                (substation["type_s"] != nb_s)
+                * (substation["type_s"] == s)
+                * s_type[s]["cost"]
+            )  # problème quand la substation n'est pas construite
+            c_cost += (substation["type_s"] != nb_s) * (
+                land_s_cables[s]["fixed_cost"]
+                + land_s_cables[s]["variable_cost"]
+                * np.sqrt((s_loc[s]["x"]) ** 2 + (s_loc[s]["y"]) ** 2)
+            )
+    z_id = -1
+    for z_cable in z_cables:
+        z_id += 1
+        for k in range(nb_s):
+            c_cost += (z_cable["s_id"] == k) * (
+                param["fixed_cost_cable"]
+                + param["variable_cost_cable"]
+                * np.sqrt(
+                    (s_loc[k]["x"] - wind_turbines[z_id]["x"]) ** 2
+                    + (wind_turbines[z_id]["y"] - s_loc[k]["y"]) ** 2
+                )
+            )
+    print("c_cost", c_cost)
+    return c_cost
+
+
+def op_cost():
+    return 0
+
+
+def cost_function(z_cables, substations):
+    return op_cost(substations, z_cables) + const_cost(substations, z_cables)
+
+
 def cplexsolve():
     # MODEL
     model = CpoModel(name="sujet6-kiro")
@@ -54,9 +91,7 @@ def cplexsolve():
         for i in range(nb_substations)
     ]
 
-<<<<<<< HEAD
-=======
-     # CONSTRAINTS
+    # CONSTRAINTS
 
     for z_cable in z_cables:
         model.add(
@@ -79,30 +114,16 @@ def cplexsolve():
     model.add(z_cable["s_id"] <= nb_s for z_cable in z_cables)
 
     # COST
-        #construction_cost
-    c_cost=0
-            #construction substation
-    for sp,substation in enumerate(substations):
-        for s in range(nb_s):            
-            c_cost+=(substation["type_s"]!=nb_s)*(substation["type_s"]==s)*s_type[s]["cost"] #problème quand la substation n'est pas construite
-            c_cost+= (substation["type_s"]!=nb_s)*(land_s_cables[s]["fixed_cost"]+land_s_cables[s]["variable_cost"]*np.sqrt((s_loc[s]["x"])**2+(s_loc[s]["y"])**2))
-    z_id=-1
-    for z_cable in z_cables:
-        z_id+=1
-        for k in range(nb_s):
+    # construction_cost
 
-            c_cost+=(z_cable["s_id"]==k)*(param["fixed_cost_cable"]+param["variable_cost_cable"]*np.sqrt((s_loc[k]["x"]-wind_turbines[z_id]["x"])**2+(wind_turbines[z_id]["y"]-s_loc[k]["y"])**2))
-    print(c_cost)    
+    # construction cable turbine
 
-                #construction cable turbine
+    # construction cable land
 
-            #construction cable land
+    # construction cable inter sub
 
-            #construction cable inter sub
-
-    #OPTIMIZE
-    model.minimize(c_cost)
->>>>>>> 153bfcf52707dece824ae32bc3ff82d0ec5da668
+    # OPTIMIZE
+    model.minimize(cost_function(z_cables, substations))
     # SOLVE
     model.solve(TimeLimit=10)
 
